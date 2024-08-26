4d42aa88170e113884546a6e995025649fefa3ba
aarav0180 <aarav.18o2005@gmail.com>
2024-08-27 00:44:49 +0530

    Compiler version added

--- saddle.config.js
+++ saddle.config.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-  // solc: "solc",                                          // Solc command to run
+  solc: "solc@0.8.10",                                          // Solc command to run
   solc_args: [                                              // Extra solc args
     '--allow-paths','contracts,tests/Contracts',
-    '--evm-version', 'istanbul'
+    '--evm-version', 'istanbul',
   ],
   solc_shell_args: {                                        // Args passed to `exec`, see:
     maxBuffer: 1024 * 500000,                               // https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
