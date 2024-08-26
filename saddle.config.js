dbe8121dbf4e9cf31c4d55f2348e5d54d1209218
aarav0180 <aarav.18o2005@gmail.com>
2024-08-27 00:55:04 +0530

    Changes improved

--- saddle.config.js
+++ saddle.config.js
@@ -1,9 +1,10 @@
 
 module.exports = {
-  solc: "solc@0.8.10",                                          // Solc command to run
+  //solc: "solc@0.8.10",                                          // Solc command to run
   solc_args: [                                              // Extra solc args
     '--allow-paths','contracts,tests/Contracts',
     '--evm-version', 'istanbul',
+    '--solidity-version', '0.8.10'
   ],
   solc_shell_args: {                                        // Args passed to `exec`, see:
     maxBuffer: 1024 * 500000,                               // https://nodejs.org/api/child_process.html#child_process_child_process_spawn_command_args_options
